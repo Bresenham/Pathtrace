df0275b6482e74444669118617a1d1a26b8e342d
Bresenham <gaul@hm.edu>
2019-09-21 10:28:09 +0200

    Switched from View to SurfaceView

--- app/src/main/java/android/app/gui/Screen.kt
+++ app/src/main/java/android/app/gui/Screen.kt
@@ -3,20 +3,41 @@ package android.app.gui
 import android.content.Context
 import android.graphics.Canvas
 import android.graphics.Paint
+import android.graphics.PixelFormat
 import android.util.AttributeSet
-import android.view.View
+import android.view.SurfaceHolder
+import android.view.SurfaceView
+import kotlin.math.min
 
-class Screen : View {
+class Screen : SurfaceView, SurfaceHolder.Callback {
 
-    constructor(context: Context, attr: AttributeSet) : super(context, attr)
+    private val paint : Paint = Paint().apply{
+        setARGB(255, 255, 0, 0)
+    }
 
+    constructor(context: Context, attr: AttributeSet) : super(context, attr) {
+        holder.addCallback(this)
+        setZOrderOnTop(true)
+        holder.setFormat(PixelFormat.TRANSLUCENT)
+    }
 
-    override fun onDraw(canvas: Canvas) {
-        canvas.drawCircle(  (canvas.width / 2).toFloat(),
-                            (canvas.height / 2).toFloat(),
-                            (Math.min(canvas.width / 2, canvas.height) / 2).toFloat(),
-                            Paint().apply{
-                                setARGB(255, 255, 0, 0)
-                            }  )
+    override fun surfaceCreated(p0: SurfaceHolder?) {
+        val ctx = holder.lockCanvas()
+        ctx.drawCircle(
+            (width / 2).toFloat(),
+            (height / 2).toFloat(),
+            (min(width / 2, height / 2)).toFloat(),
+            paint
+        )
+
+        holder.unlockCanvasAndPost(ctx)
+    }
+
+    override fun surfaceChanged(p0: SurfaceHolder?, p1: Int, p2: Int, p3: Int) {
+
+    }
+
+    override fun surfaceDestroyed(p0: SurfaceHolder?) {
+
     }
 }

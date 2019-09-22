c0ef8fb85277ad5114ff9e546a49e1a66f5bc566
Bresenham <gaul@hm.edu>
2019-09-22 15:56:58 +0200

    Updated scene

--- app/src/main/java/android/app/pathtracer/Mesh.kt
+++ app/src/main/java/android/app/pathtracer/Mesh.kt
@@ -2,7 +2,13 @@ package android.app.pathtracer
 
 class Mesh(val name : String, vararg val renderObjs : RenderObject) {
 
-    fun setAsLight() {
+    fun setAsLight() : Mesh {
         renderObjs.forEach { obj -> obj.isLight = true }
+        return this
+    }
+
+    fun setColor(col : Col) : Mesh {
+        renderObjs.forEach { obj -> obj.col = col }
+        return this
     }
 }
